d60437ae31d3b1f99a5de73384e7e8dcc6cd273b
23DP4MMisk <166586901+23DP4MMisk@users.noreply.github.com>
2024-10-03 16:14:48 +0000

    Only positives

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,44 +6,27 @@ public class App
 {
     public static void main( String[] args )
     {
-        // Kods
+      // Kods
 
-        // Ctrl + /
-        System.out.println("Hello world");
-        Scanner scanner = new Scanner(System.in);
+      // Ctrl + /
+      System.out.println("Hello world");
+      Scanner scanner = new Scanner(System.in);
 
-      //int age = 15;
-
-       // if (age >= 15) {
-        // System.out.println("Pilngadigs");
-      // } else if (age == 7) {
-        // System.out.println("Tu esi pirmklasnieks");
-      // }
-      
-      System.out.println("Value of the gift?");
-
-      int gift = Integer.valueOf(scanner.nextLine());
-
-      double tax1 = (100 + (gift - 5000) * 0.08);
-      double tax2 = (1700 + (gift - 25000) * 0.10);
-      double tax3 = (4700 + (gift - 55000) * 0.12);
-      double tax4 = (22100 + (gift - 200000) * 0.15);
-      double tax5 = (142100 + (gift - 1000000) * 0.17);
-      
-      
-      if (gift >= 5000 && gift < 25000) {
-        System.out.println("Tax: " + tax1);
-      } else if (gift >= 25000 && gift < 55000 ) {
-        System.out.println("Tax: " + tax2);
-      } else if (gift >= 55000 && gift < 200000) {
-        System.out.println("Tax: " + tax3);
-      } else if (gift >= 200000 && gift < 1000000) {
-        System.out.println("Tax: " + tax4);
-      } else if (gift >= 1000000) {
-        System.out.println("Tax: " + tax5);
-      } else {
-        System.out.println("No tax!");
+      while (true) {
+        System.out.println("Give a number:");
+        int number = Integer.valueOf(scanner.nextLine());
+        
+        if (number > 0) {
+          System.out.println(number * number);
+        }
+        if (number < 0) {
+          System.out.println("Unsuitable number");
+        }
+        if (number == 0) {
+          break;
+        }
       }
+      
 
     }
 }
