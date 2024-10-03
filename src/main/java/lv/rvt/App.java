ae95992348c72f854ce2f6a11a9770ec462867cd
23DP4MMisk <166586901+23DP4MMisk@users.noreply.github.com>
2024-10-03 17:46:30 +0000

    Numbers and sum of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,29 +4,49 @@ import java.util.*;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-      // Kods
+  public static void main( String[] args )
+  {
+    // Kods
 
-      // Ctrl + /
-      System.out.println("Hello world");
-      Scanner scanner = new Scanner(System.in);
+    // Ctrl + /
+    System.out.println("Hello world");
+    Scanner scanner = new Scanner(System.in);
+    int skaits = 0;
+    int summ = 0;
+   
 
-      while (true) {
-        System.out.println("Give a number:");
-        int number = Integer.valueOf(scanner.nextLine());
-        
-        if (number > 0) {
-          System.out.println(number * number);
-        }
-        if (number < 0) {
-          System.out.println("Unsuitable number");
-        }
-        if (number == 0) {
-          break;
-        }
+    while (true) {
+      
+      System.out.println("Give a number");
+      int number = Integer.valueOf(scanner.nextLine());
+
+      if (number == 0) {
+       break;
       }
+     
+      if (number > 0) {
+       skaits = skaits + 1;
+       summ = summ + number;
+      }
+   
+      if (number < 0) {
+       skaits = skaits + 1;
+       summ = summ + number;
+      } 
+
+    
+     
+    }
+    
+    if (skaits != 0) {
+     
+      System.out.println("Number of numbers: " + skaits);
+      System.out.println("Sum of the numbers: " + summ);
+    }
+   
+  }
+
       
 
-    }
-}
+}  
+
