5143c870ce659decb5963755e5b5201d4b8251b5
23DP4MMisk <166586901+23DP4MMisk@users.noreply.github.com>
2024-10-10 17:32:37 +0000

    Counting to hundred

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,17 +9,24 @@ public class App
     // Kods
 
     // Ctrl + /
-    System.out.println("Hello world");
+   // System.out.println("Hello world");
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Give a number: " );
+   // System.out.print("Give a number: " );
     
+    //int num = Integer.valueOf(scanner.nextLine());
+    //int factorial = 1;
+    //for (int i = 1; i <= num; i++){
+    //  factorial *= i;
+    //}
+    
+    //System.out.println("Factorial: " + factorial);
+
     int num = Integer.valueOf(scanner.nextLine());
-    int factorial = 1;
-    for (int i = 1; i <= num; i++){
-      factorial *= i;
+    int end = 100;
+
+    for(int i = num; i <= end; i++) {
+      System.out.println(i);
     }
-    
-    System.out.println("Factorial: " + factorial);
   
   
   }
