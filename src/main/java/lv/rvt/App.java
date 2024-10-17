3d7ff6d69d2ce8c202e3d51143c3765348cf626e
23DP4MMisk <166586901+23DP4MMisk@users.noreply.github.com>
2024-10-17 11:13:39 +0000

    Divisible by three

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,55 +4,25 @@ import java.util.*;
 
 public class App 
 {
-  public static void main( String[] args )
-  {
-    // Kods
-
-    // Ctrl + /
-   // System.out.println("Hello world");
-   // FACTORIAL
-   // System.out.print("Give a number: " );
-   //int num = Integer.valueOf(scanner.nextLine());
-    //int factorial = 1;
-    //for (int i = 1; i <= num; i++){
-    //  factorial *= i;
-    //}
-    //System.out.println("Factorial: " + factorial);
-    // uzd from where to where (2 parts) - 2 variants
-    //int sk = num2;
-    // while(sk <= num){
-      //System.out.println(sk);
-      //sk++;
-    //}
-    //while(sk >= num){
-      //break;
-    //}
-   
+  
+  
+  public static void main( String[] args ){
    
     Scanner scanner = new Scanner(System.in);
-   
-
-    System.out.print("Where to?");
-    int num = Integer.valueOf(scanner.nextLine());
-   
-    System.out.print("Where from?");
-    int num2 = Integer.valueOf(scanner.nextLine());
     
-    for(int sk = num2; sk <= num; sk++) {
-      System.out.println(sk);
+    divisibleByThreeInRange(2, 10);
+    
+  }
+    
+    
+  public static void  divisibleByThreeInRange(int start, int end) {
+    for(int i = start; i <= end; i++){
+      if (i % 3 == 0) {
+        System.out.println(i);
+      }
     }
-
-    for(int sk2 = num2; sk2 >= num;) {
-      break;
-    }
-    
-    
-   
-   
-
   }
 
   
-
-}  
+} 
 
