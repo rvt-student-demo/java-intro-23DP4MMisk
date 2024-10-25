a5cb04d17f609e653b309e32c9243a05449aa0c6
23DP4MMisk <166586901+23DP4MMisk@users.noreply.github.com>
2024-10-25 09:17:01 +0000

    Star sign(part3)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,21 +9,26 @@ public class App
   public static void main( String[] args ){
    
     Scanner scanner = new Scanner(System.in);
-    printRectangle(17, 3);
+    printTriangle(4);
+    
     
   }
   
-  public static void printRectangle(int width, int height) {
-    for(int i = 0; i < height; i++){
-      for(int k = 0; k < width; k++){
-        System.out.print("*");
-      }
-      System.out.println("");
-
+  public static void printTriangle(int size) {
+    for(int i = 0; i < size; i++){
+      printStars(i);
     }
+    
+    
+
+
+     
+    
 
     
   }
+
+  
 
     
   
