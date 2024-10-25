77c1d6d9a7ebf993f4403a28ebb5af57af02a275
23DP4MMisk <166586901+23DP4MMisk@users.noreply.github.com>
2024-10-25 10:27:49 +0000

    Advanced astrology(3 parts

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -10,30 +10,47 @@ public class App
    
     Scanner scanner = new Scanner(System.in);
     printTriangle(4);
+    christmasTree(4);
+    christmasTree(10);
     
   }
   
   public static void printTriangle(int size) {
     for (int i = 1; i <= size; i++) {
-        printSpaces(size - i); // Print spaces
-        printStars(i);         // Print stars
-        System.out.println();  // Move to the next line
+        printSpaces(size - i); 
+        printStars(i);         
+        System.out.println();  
     }
  }
 
  public static void printSpaces(int count) {
     for (int j = 0; j < count; j++) {
-        System.out.print(" "); // Print a space
+        System.out.print(" "); 
     }
  } 
 
  public static void printStars(int count) {
     for (int k = 0; k < count; k++) {
-        System.out.print("*"); // Print a star
+        System.out.print("*"); 
     }
   }
 
+  public static void christmasTree(int height) {
+    for (int k = 1; k <= height; k++) {
+      printSpaces(height - k);
+      printStars(2 * k - 1);
+      System.out.println();
+    }
     
+    for(int k = 0; k < 2; k++) {
+      printSpaces(height - 2);
+      printStars(3);
+      System.out.println();
+    }  
+  
+  }
+
+
   
 
   
