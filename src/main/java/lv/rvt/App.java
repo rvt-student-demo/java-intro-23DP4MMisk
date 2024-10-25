650ad33871a16aec7dcabfd19573762b8c4d8aee
23DP4MMisk <166586901+23DP4MMisk@users.noreply.github.com>
2024-10-25 09:48:08 +0000

    Printing a right-learning triangle

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -14,17 +14,22 @@ public class App
   }
   
   public static void printTriangle(int size) {
-    for(int i = 1; i < size; i++){
-      System.out.println("*");
-      printStars(i);
+    for (int i = 1; i <= size; i++) {
+        printSpaces(size - i); // Print spaces
+        printStars(i);         // Print stars
+        System.out.println();  // Move to the next line
     }
+ }
 
-    
-  }
+ public static void printSpaces(int count) {
+    for (int j = 0; j < count; j++) {
+        System.out.print(" "); // Print a space
+    }
+ } 
 
-  public static void printStars(int size){
-    for(int j = 1; j <= size; j++){
-      System.out.print("*");
+ public static void printStars(int count) {
+    for (int k = 0; k < count; k++) {
+        System.out.print("*"); // Print a star
     }
   }
 
