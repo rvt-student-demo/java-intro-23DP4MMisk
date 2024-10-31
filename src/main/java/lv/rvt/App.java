4573e015982cccec0a5c918d70d5b733bac49994
23DP4MMisk <166586901+23DP4MMisk@users.noreply.github.com>
2024-10-31 13:08:22 +0000

    two arrays

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,16 +7,36 @@ public class App
   
   
   public static void main( String[] args ){
-    int[] val = {0, 1, 2, 3}; 
+   // int[] val = {0, 1, 2, 3}; 
     int sum = 0;
+    // Uzzināt elementu skaitu masīva ?
+    //for(int i = 0; i < val.length; i++){
+      //System.out.println(val[i]);
+   // }
     
-    
-    for (int num : val) {
-      sum += num; 
-    }
+   // for (int num = 0; num < val.length; num++) {
+     // sum += val[num];
+    //}
 
+   // System.out.println("Sum of all numbers = " + sum);
+
+    int[] val = {13, -4, 82, 17}; 
+    int[] twice;
+    twice = new int[val.length];
+
+    for(int i = 0; i < val.length; i++){
+      twice[i] = val[i] * 2;
+    }
     
-    System.out.println("Sum of all numbers = " + sum);
+    System.out.println( "Original Array: " 
+        + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+ 
+    
+
+
+ 
+    System.out.println( "New Array: " 
+        + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
 
     
   }
