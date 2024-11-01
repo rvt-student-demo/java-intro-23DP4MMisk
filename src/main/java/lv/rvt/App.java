be9da889a96102453e7b3e22152215ee1ff73abe
23DP4MMisk <166586901+23DP4MMisk@users.noreply.github.com>
2024-11-01 10:56:57 +0000

    Reverse order correct

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,6 +8,9 @@ public class App
     int[] val = {0, 1, 2, 3};
     int temp;
 
+    System.out.println( "Original Array: " 
+    + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+    
     for(int i = 0; i < val.length / 2; i++){
       temp = val[i];
       val[i] = val[val.length - 1 - i];
@@ -15,9 +18,7 @@ public class App
     }
     
     
-    System.out.println( "Original Array: " 
-    + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
-    
+   
     System.out.println( "Reversed Array: " 
         + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
     
