a8c864e8b37b12537075351acb0b2c5c0dbd6b6f
23DP4MMisk <166586901+23DP4MMisk@users.noreply.github.com>
2024-12-06 07:46:20 +0000

    Projectfix

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,60 +1,20 @@
 package lv.rvt;
 
+import java.io.BufferedReader;
 import java.util.*;
-
 
 
 public class App  
 { 
-  public static void main( String[] args ){
-  Scanner scanner = new Scanner(System.in);
-    //int x = 10;
-    //int y = x;
+  public static void main( String[] args ) throws Exception {
+    Scanner scanner = new Scanner(System.in);
+    BufferedReader reader = Helper.getReader("Person.csv");
 
-    //x = 20;
-   // System.out.println(y);
-   // modify(y);
-
-    //System.out.println(y);
-
-    HeathStation childrensHospital = new HeathStation();
-
-    Person ethan = new Person("Ethan", 1, 110, 7);
-    Person peter = new Person("Peter", 33, 176, 85);
-
-    System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
-    System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
-
-    childrensHospital.feed(ethan);
-    childrensHospital.feed(ethan);
-    childrensHospital.feed(ethan);
-
-    System.out.println("");
-
-    System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
-    System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
-
-    System.out.println("weighings performed: " + childrensHospital.weighings());
-
-    childrensHospital.weigh(ethan);
-    childrensHospital.weigh(peter);
-
-    System.out.println("weighings performed: " + childrensHospital.weighings());
-
-    childrensHospital.weigh(ethan);
-    childrensHospital.weigh(ethan);
-    childrensHospital.weigh(ethan);
-    childrensHospital.weigh(ethan);
-
-    System.out.println("weighings performed: " + childrensHospital.weighings());
-    
-    
-  }
-
-  //public static void modify(int value){
-    //value += 5;
-  //}
+   System.out.println(reader.readLine());
+   System.out.println(reader.readLine());
+   System.out.println(reader.readLine());
 
   
-} 
+  }
+}
 
