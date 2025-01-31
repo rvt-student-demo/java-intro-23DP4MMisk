1b2e6d4163663e9492ef2e8afbfb991e5aa72cdb
23DP4MMisk <166586901+23DP4MMisk@users.noreply.github.com>
2025-01-31 14:09:52 +0000

    Warehouse, step 1

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,42 +8,15 @@ public class App
 { 
   public static void main( String[] args ) throws Exception {
     Scanner scanner = new Scanner(System.in);
-    
+    ProductWarehouse juice = new ProductWarehouse("Juice", 1000.0);
+    juice.addToWarehouse(1000.0);
+    juice.takeFromWarehouse(11.3);
+    System.out.println(juice.getName()); 
+    System.out.println(juice);   
     
    
-    
-    Student ollies = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
-    System.out.println(ollies);
-    ollies.study();
-    System.out.println(ollies);
-    System.out.println();
-    Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
-    Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
-    System.out.println(ada);
-    System.out.println(esko);
-
-    Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
-
-    int i = 0;
-    while (i < 25) {
-      ollie.study();
-      i = i + 1;
-    }
-    System.out.println(ollie);
-
-    ArrayList<Personn> persons = new ArrayList<Personn>();
-    persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
-    persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
-
-    printPersons(persons);
 
   }
-
-  public static void printPersons(ArrayList<Personn> persons) {
-    for (Personn person : persons) {
-        person.printDetails();
-    }
-}
 
 
   
