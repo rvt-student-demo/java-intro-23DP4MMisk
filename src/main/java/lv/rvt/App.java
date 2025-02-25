f5f7e144b554f6319622590c43d147e4cb4560a3
23DP4MMisk <166586901+23DP4MMisk@users.noreply.github.com>
2025-02-25 07:20:35 +0000

    Table

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,17 +9,44 @@ public class App
   public static void main( String[] args ) throws Exception {
     Scanner scanner = new Scanner(System.in);
    
-    System.out.println(
-      // ConsoleColors.BLUE_BACKGROUND + "                    "
-      //ConsoleColors.GREEN_BACKGROUND + ConsoleColors.YELLOW + "Hello worlds"
-      // ConsoleColors.YELLOW + ConsoleColors.GREEN_BACKGROUND + "Hello world"
-      // + ConsoleColors.RESET
-    );
+    List<Integer>Array = new ArrayList<>();
+    System.out.println("Ievadi skaitļus");
+    
+    while(true){
+     String input = scanner.nextLine();
+     if(input.isEmpty()) {
+      break;
+     }
+     try {
+      int number = Integer.parseInt(input);
+      Array.add(number);
+     } catch (NumberFormatException e) {
+      System.out.println("Lūdzu ievadi tikai veselus skaitļus");
+     }
+     
+    }
 
-    // System.out.println("djkdjkjd");
+    Array.sort(Comparator.naturalOrder());
+    System.out.println("\nSakartots augoša secība:");
+    printTable(Array);
+
+    Array.sort(Comparator.reverseOrder());
+    System.out.println("\nSakartots dilstoša seciba:");
+    printTable(Array);
+
+    
+
   }
-
-
   
+  public static void printTable(List<Integer> Array){
+    System.out.println("=================");
+    System.out.println("| index | value |");
+    System.out.println("=================");
+    for(int i = 0; i < Array.size(); i++){
+      System.out.printf("| %5d | %5d |\n", i, Array.get(i));
+    }
+    System.out.println("=================");
+  }
 }
+
 
